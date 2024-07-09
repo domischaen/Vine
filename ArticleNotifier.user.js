58036e57c2b3b7372bde9b3dcd7ef728abc2253c
domischaen <domischaen@gmail.com>
2024-07-09 20:47:43 +0200

    Update ArticleNotifier.user.js

--- ArticleNotifier.user.js
+++ ArticleNotifier.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Vine Fuckers
 // @namespace    http://tampermonkey.net/
-// @version      1.5.19
+// @version      1.5.20
 // @updateURL    https://raw.githubusercontent.com/domischaen/Vine/main/ArticleNotifier.user.js
 // @downloadURL  https://raw.githubusercontent.com/domischaen/Vine/main/ArticleNotifier.user.js
 // @description  Vine Fuckers
